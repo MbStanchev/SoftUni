6114ac55a3d1f4da63c022bcd1289c998ab13d41
Stanchev.ny <mbstanchev@gmail.com>
2022-06-06 18:59:15 +0300

    new problems in l

--- Fundamentals/LAB/L3/courses.py
+++ Fundamentals/LAB/L3/courses.py
@@ -1,6 +1,6 @@
 n = int(input())
 list_of_names = []
 for i in range(n):
-    names = input()
-    list_of_names.append(names)
+    # names = input()
+    list_of_names.append(input())
 print(list_of_names)
