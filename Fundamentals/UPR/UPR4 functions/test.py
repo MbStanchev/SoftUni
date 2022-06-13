04d01733c2c266c0a75a337960ffe8ce4ef9619b
Stanchev.ny <mbstanchev@gmail.com>
2022-06-13 13:46:50 +0300

    new problems in upr

--- Fundamentals/UPR/UPR4 functions/test.py
+++ Fundamentals/UPR/UPR4 functions/test.py
@@ -2,11 +2,11 @@ word = input()
 
 
 dig_couter = 0
-for i in word:
-    if i.isdigit():
+for char in word:
+    if char.isdigit():
         dig_couter += 1
-    if dig_couter > 2:
-        print(True)
-    else:
-        print("Password must have at least 2 digits")
-
+if dig_couter >= 2:
+    print("y")
+else:
+    print("Password must have at least 2 digits")
+    print("n")
