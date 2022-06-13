dfff62bc70bb73aff5e8499643518102cad2fa17
Stanchev.ny <mbstanchev@gmail.com>
2022-06-13 12:42:03 +0300

    new problems in upr

--- Fundamentals/UPR/UPR4 functions/password_validator.py
+++ Fundamentals/UPR/UPR4 functions/password_validator.py
@@ -9,7 +9,7 @@ def password_lenth_validator(word):
         return  False
 
 def pass_is_all_num_sim(word):
-    if not word.isalnum():
+    if word.isalnum():
         return True
     else:
         print("Password must consist only of letters and digits")
@@ -17,18 +17,16 @@ def pass_is_all_num_sim(word):
 
 def two_digits(word):
     dig_couter = 0
-    for i in word:
-        if i.isdigit():
+    for char in word:
+        if char.isdigit():
             dig_couter += 1
     if dig_couter >= 2:
         return True
-
-    print("Password must have at least 2 digits")
-    return False
+    else:
+        print("Password must have at least 2 digits")
+        return False
 
 # if password_lenth_validator(password) and two_digits(password) and pass_is_all_num_sim(password):
 valid = [password_lenth_validator(password), pass_is_all_num_sim(password), two_digits(password)]
 if all(valid):
-    print("We have a perfect number!")
-else:
-    print("It's not so perfect.")
+    print("Password is valid")
