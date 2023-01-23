bb829e4552b5521991c1daea582f47968e31ac5c
Stanchev.ny <mbstanchev@gmail.com>
2023-01-23 20:13:21 +0200

    multidimensional_lists-first_exercise

--- Advanced/LAB/multidimensional_lists/square_with_maximum_sum.py
+++ Advanced/LAB/multidimensional_lists/square_with_maximum_sum.py
@@ -14,9 +14,11 @@ for row in range(n - 1):
             biggest_sum = current_sum
             r = row
             c = col
+
 for e in range(r, r + 1):
-    for t in range(c, c +1):
+    for t in range(c, c + 1):
         res.append(matrix[e][t])
+
 print(f'{matrix[r][c]} {matrix[r][c+1]}')
 print(f'{matrix[r+1][c]} {matrix[r+1][c+1]}')
 
