cd49fe89088b7677e6dd8869b0719fb92e566c1b
Stanchev.ny <mbstanchev@gmail.com>
2023-02-10 22:56:43 +0200

    file_handling_not_working

---
+++ Advanced/Exercises/file_handling/even_lines.py
@@ -0,0 +1,14 @@
+simb = ["-", ",", ".", "!", "?"]
+
+with open('SoftUni/Advanced/Exercises/file_handling/f.txt', 'r') as even_lines_file:
+    f = even_lines_file.readlines()
+
+print(f)
+
+for row in range(0, len(f), 2):
+    for sybol in simb:
+        f[row] = f[row].replace(sybol, '@')
+    print(f)
+# file = open("files/text.txt", "r")
+# t = file.read()
+# print(t)
